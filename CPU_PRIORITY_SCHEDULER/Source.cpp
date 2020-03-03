72940816519e43291abf1ad361f20e0f0c97dea5
Ryan Dobbe <rjd029@shsu.edu>
2020-03-03 03:15:31 -0600

    v1.0

--- CPU_PRIORITY_SCHEDULER/Source.cpp
+++ CPU_PRIORITY_SCHEDULER/Source.cpp
@@ -2,22 +2,21 @@
 #include <fstream>
 #include <string>
 #include <stdlib.h>
-//#include "stdc++.h"
+#include <queue>
+/*************************************************************************************************
+									CPU_PRIORITY_SCHEDULER
+									@Author@ - Ryan Dobbe
+this program takes in process inputs from a file and schedules time in a preemptive priority
+secondary Round Robin with quantum initially set to 2.  The quantum value is adjustable for the 
+User.  Output of the process execution as well as turnaround times and total avg turnaround times
+are shown on the consle. Input file is int the format:
+							Arrival --->  Burst  --->   Priority
+							0				2				2
+**************************************************************************************************/
 
 using namespace std;
 
-//Structures
-struct Process {
-	int pid;
-	int arrival;
-	int burst;
-	int priority;
-	int ex_time;
-};
-
 //Globals
-bool done = false;
-int _time = 0;
 const int NUM_PROCESSES = 100;
 
 //Functions
@@ -25,7 +24,6 @@ void file_read();
 
 int main() {
 	file_read();
-	system("pause");
 	return 0;
 }
 
@@ -39,75 +37,84 @@ void file_read() {
 		exit(1);
 	}
 
-	int num[150], arrival[150], burst[150], priority[150];
-	int quantum = 2;
+	//Read process.txt into memory
+	int arrival[100];
+	int burst[100];
+	int priority[100];
+	queue <int> circ_buff;
+	int exc_time[100];
+	int tat_time[100];
+	int tat_total = 0;
+	int att_time;
 
-	//Read file line by line & set  process arrays
-	for (int i = 1; i < NUM_PROCESSES ;) {
-		Process proc[102] = { arrival[i], burst[i], priority[i] };
-		while (pr_File >> arrival[i] >> burst[i] >> priority[i]) {
-			cout << "Process: " << i << " " << "| Arrival: " << arrival[i] << " " << "| Burst: " << burst[i] << " " << "| Prority: " << priority[i] << " " << '\n';
-			proc[i].pid = i;
-			proc[i].arrival = arrival[i];
-			proc[i].burst = burst[i];
-			proc[i].priority = priority[i];
-			i++;
-		}
-		i = 1;
-		cout << proc[i].pid << " " << proc[i].arrival << " " << proc[i].burst << " " << proc[i].priority << " " << endl << '\n';
-		pr_File.close();
-		
-		if (i == 1) {
-			
-			proc[i].burst = proc[i].burst - quantum;
-			_time = _time + quantum;
-
-			if (proc[i].burst == 0) {
-				done = true;
-			}
-			else if (proc[i].burst < 0) {
-				done = true;
-				proc[i].burst = 0;
-				proc[i].ex_time = _time;
-				_time = _time + (-1 * proc[i].burst);
-				
-			}
-			if (done) {
-				cout << "Process: " << proc[i].pid << " Finished at time: " << proc[i].ex_time << endl;
-			}
-			cout << "Current Time: " << _time << endl;
-			i++;
-			system("pause");
-
-		}
-		else {
-		
-			if (_time < proc[i].arrival) {
-				
-				if (proc[i - 1].burst > 0) {
-					proc[i - 1].burst = proc[i - 1].burst - quantum;
-				}
-				_time = _time + 1;
-			}
-			else  if (_time >= proc[i].arrival) {
-
-
-				if (proc[i].priority > proc[i + 1].priority) {
-					proc[i + 1].burst = proc[i + 1].burst - quantum;
-				}
-				else if (proc[i].priority < proc[i + 1].priority) {
-					proc[i].burst = proc[i].burst - quantum;
-				}
-
-
-				if (done) {
-					cout << "Process: " << proc[i].pid << " Finished at time: " << proc[i].ex_time << endl;
-					done = false;
-				}
-			}
-		}
-		i++;
+	for (int i = 0; i < NUM_PROCESSES; i++) {
+		pr_File >> arrival[i] >> burst[i] >> priority[i];
 	}
+
+	//Apply priority scheduler
+	int time = 0;
+	int quantum = 2;
+	int num_processes_left = NUM_PROCESSES;
+	int cp_quantum = 0;
+	//Loop
+	while (num_processes_left > 0) {
+
+		//Add processes with arrival equal to time to a "buffer"
+		for (int i = 0; i < NUM_PROCESSES; i++) {
+			if (time == arrival[i]) {
+				circ_buff.push(i);
+			}
+		}
+
+		//If another process is in the queue
+		if (circ_buff.size() <= 0) {
+			//Empty Queue
+			cp_quantum = 0;
+			time++;
+
+		}else {
+			//Move process to back of queue when quantum threshold is hit
+			if (cp_quantum >= quantum && circ_buff.size() > 1) {
+				//Multiple in queue
+				int temp = circ_buff.front();
+				circ_buff.pop();
+				circ_buff.push(temp);
+				cp_quantum = 0;
+			}
+
+
+			//Process (step time)
+			burst[circ_buff.front()] = burst[circ_buff.front()] - 1;
+			cp_quantum++;
+			time++;
+			cout << "Processing " << circ_buff.front() + 1 << "	Q = " << cp_quantum << endl;
+
+			//Remove Finished Processes
+			if (burst[circ_buff.front()] <= 0) {
+				//Calculate turn around time for each process
+				exc_time[circ_buff.front()] = time;
+				tat_time[circ_buff.front()] = exc_time[circ_buff.front()] - arrival[circ_buff.front()];
+				cout << "Process " << circ_buff.front() + 1 << " has finished at time " << time << endl;
+				cout << "Process " << circ_buff.front() + 1 << " Turnaround Time: " << tat_time[circ_buff.front()] << endl;
+				circ_buff.pop();
+				num_processes_left--;
+				cp_quantum = 0;
+				
+			}
+
+		}
+
+	}
+	pr_File.close();
+	
+	//Calculate Avg time
+	for (int i = 0; i < NUM_PROCESSES; i++) {
+		tat_total = tat_total + tat_time[i];
+	}
+
+	att_time = tat_total / NUM_PROCESSES;
+
+	cout << "Average Turnaround Time : " << att_time << endl;
 	
 }
 
